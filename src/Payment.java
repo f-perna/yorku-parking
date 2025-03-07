062910963116c0be53c19c728be0bd4974e8b8be
Dylan Gauvin <73555281+Dylfive@users.noreply.github.com>
2025-03-07 18:50:21 -0500

    Create Payment.java
    Just added base payment java file

---
+++ src/Payment.java
@@ -0,0 +1,21 @@
+public abstract class Client {
+	private int paymentID;
+  private double amount;
+  public status PaymentStatus;
+	
+	//public static enum PaymentMethod {
+	//	Credit, Debit, Mobile, Cash;
+	//}
+
+  public boolean processPayment() {
+		return false;
+	}
+
+  public void refundDeposit(){
+    
+  }
+
+  public void setPaymentMethod() {
+
+  }
+}
