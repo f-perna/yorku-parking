0991b1f7b432843d2e25563de33ba4e704677372
Dylan Gauvin <73555281+Dylfive@users.noreply.github.com>
2025-03-07 23:35:03 -0500

    Update Payment.java
    added some functionality

--- src/Payment.java
+++ src/Payment.java
@@ -1,21 +1,54 @@
-public abstract class Client {
+public abstract class Payment {
 	private int paymentID;
-  private double amount;
-  public status PaymentStatus;
-	
-	//public static enum PaymentMethod {
-	//	Credit, Debit, Mobile, Cash;
-	//}
+	private double amount;
+  	public PaymentStatus status;
+	public PaymentMethod method;
 
-  public boolean processPayment() {
-		return false;
+	public static enum PaymentStatus {
+		PENDING, COMPLETED, FAILED, REFUNDED;
+	}
+	
+	public Payment(int paymentID, double amount, PaymentMethod method) {
+		this.paymentID = paymentID;
+		this.amount = amount;
+		this.method = method;
+		this.status = PaymentStatus.PENDING;
 	}
 
-  public void refundDeposit(){
-    
-  }
+	public boolean processPayment() {
+		if (status == PaymentStatus.PENDING) {
+			// payment process
+			status = PaymentStatus.COMPLETED;
+			System.out.println("Payment processed successfully.");
+			return true;
+		}
+		return false;	
+	}
 
-  public void setPaymentMethod() {
+  	public void refundDeposit(){
+  		if (status == PaymentStatus.COMPLETED) {
+			status = PaymentStatus.REFUNDED;
+			System.out,println("Deposit refunded.");
+		}
+  	}
 
-  }
+  	public void setPaymentMethod(PaymentMethod method) {
+		this.method = method;
+  	}
+
+	public PaymentStatus getStatus() {
+		return status;
+	}
+
+	public int getPaymentID() {
+		return paymentID;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public PaymentMethod getMethod() {
+		return method;
+	}
 }
