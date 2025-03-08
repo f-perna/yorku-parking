e193ab6a8d5b99c5bce729f995ad04e5f0a38e9d
Dylan Gauvin <73555281+Dylfive@users.noreply.github.com>
2025-03-07 23:41:39 -0500

    Update PaymentMethod.java
    added functionality, still need to test it

--- src/PaymentMethod.java
+++ src/PaymentMethod.java
@@ -1,3 +1,37 @@
 public abstract class PaymentMethod {
+        protected String paymentType;
 
+        public abstract void setPaymentMethod(String method);
+
+        public String getPaymentType() {
+                return paymentType;
+        }
+}
+
+class Debit extends PaymentMethod {
+        @Override
+        public void setPaymentMethod(String method) {
+                this.paymentType = "Debit";
+        }
+}
+
+class Credit extends PaymentMethod {
+        @Override
+        public void setPaymentMethod(String method) {
+                this.paymentType = "Credit";
+        }
+}
+
+class Mobile extends PaymentMethod {
+        @Override
+        public void setPaymentMethod(String method) {
+                this.paymentType = "Mobile";
+        }
+}
+
+class Cash extends PaymentMethod {
+        @Override
+        public void setPaymentMethod(String method) {
+                this.paymentType = "Cash";
+        }
 }
