9ca4ab642f60d80ddeaa09e7d97d7f5eaf0a386f
Shayan akbarloyshabgahi <90583586+Shayanakbarloo@users.noreply.github.com>
2025-03-13 01:00:16 -0400

    Added some methods and implemented some values

--- src/Booking/Booking.java
+++ src/Booking/Booking.java
@@ -1,5 +1,22 @@
 package Booking;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Booking {
+	private int bookingId; 
+	private int parkingSpaceId; 
+	private String startTime;
+	private String endTime;
+	private double deposit; 
+	private BookingStatus status; 
+	
+	public Booking(int bookingId, int parkingSpceId, double desposit) {
+		this.bookingId = bookingId; 
+		this.parkingSpaceId = parkingSpaceId; 
+		this.deposit = deposit; 
+		this.startTime = getCurrentTime();
+		this.endTime = "";
+	}
+	
+	
 
 }
