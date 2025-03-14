9e2cdde9252e0ec9ebfd7c193e601798257e2346
Fernando Perna <113391793+f-perna@users.noreply.github.com>
2025-03-14 17:50:35 -0400

    Fix

--- src/views/BookingPage.java
+++ src/views/BookingPage.java
@@ -1,24 +1,27 @@
-package booking;
+package views;
 
 import javax.swing.*;
+
+import booking.Booking;
+
 import java.awt.*; 
 
-public class BookingGUI extends JFrame{
+public class BookingPage extends JFrame{
 	private Booking booking; 
 	private JLabel infoLabel; 
 	private JButton confirmButton; 
 	private JButton cancelButton; 
 	private JButton completeButton; 
 	
-	public BookingGUI() {
-		booking = new Booking (1, 101, 50.0); 
+	public BookingPage() {
+		//booking = new Booking (1, 101, 50.0); 
 		infoLabel = new JLabel(booking.toString());
 		
 		confirmButton = new JButton("Confirm Booking"); 
 		cancelButton = new JButton("Cancel Booking"); 
 		completeButton = new JButton ("Complete Booking"); 
 		
-		confirmButton.addActionListener(e -> {booking.confirm();updateBookingInfo();});
+		//confirmButton.addActionListener(e -> {booking.confirm();updateBookingInfo();});
 	}
 
 	
