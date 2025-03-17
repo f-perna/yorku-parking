8857f7a845dc97b7bf33dd33ad782a5230f0a376
Mouiz Ahmed <mouiza@my.yorku.ca>
2025-03-17 06:47:32 -0400

    manager login page

--- src/views/LoginPage.java
+++ src/views/LoginPage.java
@@ -1,34 +1,28 @@
 package views;
 
 import java.awt.Color;
-
-import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-
-import client.Client;
 import controllers.NavigationController;
 import parking.ParkingSystem;
 
 public class LoginPage extends JPanel {
-	
+
 	private ParkingSystem parkingSystem = ParkingSystem.getInstance();
-	
 	private JTextField emailField;
 	private JPasswordField passwordField;
 	private JLabel statusLabel;
+	private JCheckBox managerCheckBox;
 
 	public LoginPage(JFrame parent) {
 		setLayout(new GridBagLayout());
@@ -42,7 +36,7 @@ public class LoginPage extends JPanel {
 		gbc.gridy = 0;
 		add(titleLabel, gbc);
 
-		JLabel emailLabel = new JLabel("Email:");
+		final JLabel emailLabel = new JLabel("Email:");
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		add(emailLabel, gbc);
@@ -60,32 +54,47 @@ public class LoginPage extends JPanel {
 		gbc.gridx = 1;
 		add(passwordField, gbc);
 
-		JButton loginButton = new JButton("Login");
+		managerCheckBox = new JCheckBox("Manager Login");
 		gbc.gridx = 1;
 		gbc.gridy = 3;
+		add(managerCheckBox, gbc);
+
+		managerCheckBox.addActionListener((ActionEvent e) -> {
+			boolean isManager = managerCheckBox.isSelected();
+			titleLabel.setText(isManager ? "Manager Login" : "Login");
+			emailLabel.setText(isManager ? "Username:" : "Email:");
+		});
+
+		JButton loginButton = new JButton("Login");
+		gbc.gridx = 1;
+		gbc.gridy = 4;
 		add(loginButton, gbc);
 
 		statusLabel = new JLabel(" ");
 		statusLabel.setForeground(Color.RED);
-		gbc.gridy = 4;
+		gbc.gridy = 5;
 		add(statusLabel, gbc);
-		
+
 		JButton goToRegister = new JButton("Don't have an account? Click Here");
-        gbc.gridy = 0;
-        gbc.gridy = 5;
-        add(goToRegister, gbc);
-        goToRegister.addActionListener((ActionEvent e) -> NavigationController.showPage("Register"));
+		gbc.gridy = 6;
+		add(goToRegister, gbc);
+		goToRegister.addActionListener((ActionEvent e) -> NavigationController.showPage("Register"));
 
-		loginButton.addActionListener((ActionEvent e) -> handleLogin(emailField.getText(), passwordField.getText()));
+		loginButton.addActionListener((ActionEvent e) -> handleLogin(emailField.getText(),
+				new String(passwordField.getPassword()), managerCheckBox.isSelected()));
 	}
 
-	private void handleLogin(String email, String password) {
-		if (parkingSystem.login(email, password)) {
-			ClientPage clientPage = (ClientPage) NavigationController.getPage("Client");
-			clientPage.refresh();
-			NavigationController.showPage("Client");
+	private void handleLogin(String email, String password, boolean isManager) {
+		if (isManager) {
+			
+		} else {
+			if (parkingSystem.login(email, password)) {
+				ClientPage clientPage = (ClientPage) NavigationController.getPage("Client");
+				clientPage.refresh();
+				NavigationController.showPage("Client");
+			} else {
+				statusLabel.setText("Invalid credentials");
+			}
 		}
-		
 	}
-
 }
