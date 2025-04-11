13e27406abc5db4fbeca7b97763c64be177fb950
Fernando Perna <113391793+f-perna@users.noreply.github.com>
2025-04-11 17:48:31 -0400

    rename classes

--- src/test/java/randoop/models/auth/ModelsAuthRegressionTest0.java
+++ src/test/java/randoop/models/auth/ModelsAuthRegressionTest0.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class RegressionTest0 {
+public class ModelsAuthRegressionTest0 {
 
     public static boolean debug = false;
 
